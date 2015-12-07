8c2b7a58a008deee3e8e78996c2a8b281fa63405
Þórdís Bakkmann Kristinsdóttir <thordis@tbk-MacBook-Pro.lan>
2015-12-07 14:32:09 +0000

    search comyear komið

--- ui.cpp
+++ ui.cpp
@@ -151,8 +151,8 @@ void UI::searchComMenu()
     cout << "(N) Name" << endl
          << "(T) Type" << endl
          << "(C) Year of Creation" << endl
-         << "(Q) Quit program" <<endl
-         << "(M) Return to Menu"<<endl;
+         << "(M) Return to Menu" << endl
+         << "(Q) Quit program" << endl;
     cout << "Select a letter: ";
     cin >> choice;
     switch(choice)
@@ -164,7 +164,7 @@ void UI::searchComMenu()
         case 'T':   searchComType();
                     break;
         case 'c':
-        case 'C':   searchComType();
+        case 'C':   searchComYear();
                     break;
         case 'm':
         case 'M':   return;
