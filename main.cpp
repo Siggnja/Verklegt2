85d2be6dd91933aeecc7f31410124069886bc83f
sabbisun <sabbisun@gmail.com>
2015-12-08 13:43:49 +0000

    Smá breyting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <QCoreApplication>
 #include <QtSql>
 #include <iostream>
-<<<<<<< HEAD
 #include <QtSql>
-=======
->>>>>>> 78b5c1a381f1332efc35dfb47b856e52ea36b9b0
+
 #include "ui.h"
 
 using namespace std;
@@ -13,8 +11,6 @@ int main()
 {
     UI ui;
     ui.run();
-<<<<<<< HEAD
-=======
+
     return 0;
->>>>>>> 78b5c1a381f1332efc35dfb47b856e52ea36b9b0
 }
