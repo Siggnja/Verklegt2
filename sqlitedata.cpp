570967db44fbc5a0611d2f9f5545dc4366b9d0be
Þórdís Bakkmann Kristinsdóttir <thordis@tbk-MacBook-Pro.local>
2015-12-08 13:50:24 +0000

    returnar true i forid i sql ef var == id

--- sqlitedata.cpp
+++ sqlitedata.cpp
@@ -519,7 +519,7 @@ bool SQLiteData::searchForId(const int id, const string tablename)
         var = queryname.value("id").toUInt();
     }
     db.close();
-    if(var)
+    if(var == id)
     {
         return true;
     }
